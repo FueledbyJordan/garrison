2e9ddbfe72d858e75cba474ad130b54c07342cd7
Jordan <djm@murrayfoundry.com>
2020-11-22 17:02:42 -0600

    add copy, link, and delete operations to utilities.

--- lib/Utilities.hpp
+++ lib/Utilities.hpp
@@ -5,11 +5,17 @@ class Utilities
 {
 
 public:
-    static void ltrim (std::string &s);
-    static void rtrim (std::string &s);
-    static void trim (std::string &s);
-    static bool FileExists(const std::string & filePath);
+    static void ltrim(std::string &s);
+    static void rtrim(std::string &s);
+    static void trim(std::string &s);
 
+	static void Copy(const std::string & srcPath, const std::string & destPath, bool force = false);
+	static void Link(const std::string & srcPath, const std::string & destPath, bool force = false);
+	static void Delete(const std::string & path);
+
+	static std::string GetFileName(const std::string & path);
+
+    static bool FileExists(const std::string & filePath);
     static bool UserHasReadPermissions(const std::string & path);
     static bool GroupHasReadPermissions(const std::string & path);
     static bool OtherHasReadPermissions(const std::string & path);
@@ -19,6 +25,7 @@ public:
     static bool UserHasExecutePermissions(const std::string & path);
     static bool GroupHasExecutePermissions(const std::string & path);
     static bool OtherHasExecutePermissions(const std::string & path);
+
 private:
     Utilities(){};
     ~Utilities(){};
